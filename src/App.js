771668c6cdb9979fb71855187e03fe780c097765
Antonio Rosales <75122333+lethamburn@users.noreply.github.com>
2021-09-02 15:14:03 +0200

    FEAT APP WORKING

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useContext } from "react";
+import { FormattedMessage, FormattedDate } from "react-intl";
+import { Context } from "./components/Wrapper";
+function App(props) {
+  const context = useContext(Context);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <select value={context.locale} onChange={context.selectLanguage}>
+          <option value="en">English</option>
+          <option value="fr">French</option>
+          <option value="es">Spanish</option>
+        </select>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <FormattedMessage
+            id="app.header"
+            defaultMessage="Edit the  and save to reload"
+            values={{
+              fileName: "src/App.js",
+              code: (word) => <strong>{word}</strong>,
+            }}
+          />
         </p>
         <a
           className="App-link"
@@ -15,8 +30,21 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <FormattedMessage id="app.content" defaultMessage="Learn React" />
         </a>
+        <FormattedMessage
+          id="app.channel.plug"
+          defaultMessage="Internationalization for Upgraders"
+          values={{ userName: "Peter Parker" }}
+        />
+        <br />
+        <FormattedDate
+          value={props.date}
+          year="numeric"
+          month="long"
+          day="numeric"
+          weekday="long"
+        />
       </header>
     </div>
   );
